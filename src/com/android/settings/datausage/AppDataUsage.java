7e29ea3b86ea50895dba92602e093361cdeb1c8b
AlexanderR34 <oharol888@gmail.com>
2026-08-30 23:51:08 -0600

    Settings: Remove per-app network restriction and isolation features

--- src/com/android/settings/datausage/AppDataUsage.java
+++ src/com/android/settings/datausage/AppDataUsage.java
@@ -164,33 +164,6 @@ public class AppDataUsage extends DataUsageBaseFragment implements OnPreferenceC
                 if (mUnrestrictedData != null) mUnrestrictedData.setOnPreferenceChangeListener(this);
             }
 
-            AppCellularDataRestrictionPreferenceController cellularController =
-                    use(AppCellularDataRestrictionPreferenceController.class);
-            if (cellularController != null) {
-                cellularController.setUid(mAppItem.key);
-                Preference pref = findPreference("app_cellular_data_restriction");
-                if (pref != null) {
-                    cellularController.updateState(pref);
-                }
-            }
-            AppWifiDataRestrictionPreferenceController wifiController =
-                    use(AppWifiDataRestrictionPreferenceController.class);
-            if (wifiController != null) {
-                wifiController.setUid(mAppItem.key);
-                Preference pref = findPreference("app_wifi_data_restriction");
-                if (pref != null) {
-                    wifiController.updateState(pref);
-                }
-            }
-            AppNetworkIsolationPreferenceController isolationController =
-                    use(AppNetworkIsolationPreferenceController.class);
-            if (isolationController != null) {
-                isolationController.setUid(mAppItem.key);
-                Preference pref = findPreference("app_network_isolation_switch");
-                if (pref != null) {
-                    isolationController.updateState(pref);
-                }
-            }
             mDataSaverBackend = new DataSaverBackend(mContext);
 
             use(AppDataUsageListController.class).init(uidList);
