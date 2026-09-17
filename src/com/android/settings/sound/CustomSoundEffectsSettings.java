2749e2336c16ea4abb9582d90bfbc84533bbd12d
AlexanderR34 <oharol888@gmail.com>
2026-09-17 03:56:31 +0000

    Settings: Allow picking custom sounds in CustomSoundEffectsSettings
    Change-Id: Iba0250b94c137574c72ed8724ba98afcb502f352

--- src/com/android/settings/sound/CustomSoundEffectsSettings.java
+++ src/com/android/settings/sound/CustomSoundEffectsSettings.java
@@ -21,9 +21,16 @@ import android.app.settings.SettingsEnums;
 import com.android.settings.R;
 import com.android.settings.dashboard.DashboardFragment;
 
+import android.content.Intent;
+import android.os.UserHandle;
+import androidx.preference.Preference;
+import com.android.settings.RingtonePreference;
+
 public class CustomSoundEffectsSettings extends DashboardFragment {
 
     private static final String TAG = "CustomSoundEffectsSettings";
+    private static final int REQUEST_CODE = 200;
+    private RingtonePreference mRequestPreference;
 
     @Override
     protected int getPreferenceScreenResId() {
@@ -38,5 +45,30 @@ public class CustomSoundEffectsSettings extends DashboardFragment {
     @Override
     public int getMetricsCategory() {
         return SettingsEnums.SOUND;
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(Preference preference) {
+        if (preference instanceof RingtonePreference) {
+            writePreferenceClickMetric(preference);
+            mRequestPreference = (RingtonePreference) preference;
+            mRequestPreference.onPrepareRingtonePickerIntent(mRequestPreference.getIntent());
+            getActivity().startActivityForResultAsUser(
+                    mRequestPreference.getIntent(),
+                    REQUEST_CODE,
+                    null,
+                    UserHandle.of(mRequestPreference.getUserId()));
+            return true;
+        }
+        return super.onPreferenceTreeClick(preference);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (mRequestPreference != null) {
+            mRequestPreference.onActivityResult(requestCode, resultCode, data);
+            mRequestPreference = null;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
