9670d6f9a47ca3ec4b62b39f14e37e0cfebd20eb
AlexanderR34 <oharol888@gmail.com>
2026-09-21 16:54:54 +0000

    Settings: Align volume boost summary calculations with 8.0 dB max gain
    Change-Id: I293db932a82e04afea3de1517796096e6b2eee6b

--- src/com/android/settings/sound/VolumeBoostSeekBarPreference.java
+++ src/com/android/settings/sound/VolumeBoostSeekBarPreference.java
@@ -121,7 +121,7 @@ public class VolumeBoostSeekBarPreference extends SeekBarPreference {
 
     private void updateViews(int progress) {
         int displayVolumePercent = 100 + Math.round((progress / 100.0f) * 100.0f); // 100% -> 200%
-        float gainDb = (progress / 100.0f) * 10.0f; // 0 dB -> +10.0 dB
+        float gainDb = (progress / 100.0f) * 8.0f; // 0 dB -> +8.0 dB
 
         if (mValueBadgeView != null) {
             mValueBadgeView.setText(String.format(Locale.getDefault(), "%d%%", displayVolumePercent));
